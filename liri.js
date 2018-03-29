87739d57077996d78b7bfa7e2294605b2ac71de7
jkrowden <jkrowden@gmail.com>
2018-03-29 06:53:42 -0600

    Completed do-what-it-says function. Also included a blank log file

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-
+//Still working on do that thing thing
+var fs = require('fs');
 var moment = require("./moment.js");
 var request = require("request");
 var Spotify = require("node-spotify-api");
@@ -10,32 +11,38 @@ var spotify = new Spotify(keys.spotify);
 var twitter = new Twitter(keys.twitter);
 
 var command = process.argv[2];
+var argument = process.argv[3];
 
 if (process.argv.length < 3) {
     console.log('Error: No command given!');
     process.exit(1);
 }
 
-switch (command) {
-    case 'my-tweets':
-        loadTweets();
-        break;
-    case 'spotify-this-song':
-        spotifySong();
-        break;
-    case 'movie-this':
-        movieThis();
-        break;
-    case 'do-what-it-says':
-        doWhatItSays();
-        break;
-    case 'help':
-        helpText();
-        break;
-    default:
-        console.log("Command not found. Available commands:\nmy-tweets\nspotify-this-song\nmovie-this\ndo-what-it-says");
-        break;
+start();
+
+function start() {
+    switch (command) {
+        case 'my-tweets':
+            loadTweets();
+            break;
+        case 'spotify-this-song':
+            arguments.length > 0 ? spotifySong(argument) : spotifySong();
+            break;
+        case 'movie-this':
+            movieThis();
+            break;
+        case 'do-what-it-says':
+            doWhatItSays();
+            break;
+        case 'help':
+            helpText();
+            break;
+        default:
+            console.log("Command not found. Available commands:\nmy-tweets\nspotify-this-song\nmovie-this\ndo-what-it-says");
+            break;
+    }
 }
+
 
 function helpText() {
     console.log('\nWelcome to LIRI! We have the following commands available:');
@@ -65,26 +72,56 @@ function loadTweets() {
 
 }
 
-function spotifySong() {
-    if (process.argv.length < 4) {
+function spotifySong(song) {
+    var songSearch;
+
+    if (process.argv.length < 4 && arguments.length === 0) {
         console.log('No argument given for query');
         process.exit(1);
     }
 
-    var song = process.argv[3].trim();
+    if (arguments.length > 0) {
+        songSearch = song;
+    } else {
+        songSearch = process.argv[3];
+    }
 
     spotify.search({
         type: 'track',
-        query: song,
-        count: 1
+        query: songSearch,
     }).then(function (response) {
         console.log(response.tracks.items[0].artists[0].name + " - " + response.tracks.items[0].name + " || " + response.tracks.items[0].album.name + " (" + response.tracks.items[0].external_urls.spotify + ")");
     }).catch(function (error) {
+        console.log('spotify query error:');
         console.log(error);
     });
 }
 
 function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        error === true ? console.log('read file error') : '';
+
+        saysTo = data.split(',');
+        if (saysTo[saysTo.length - 1] === '') {
+            saysTo.pop();
+        }
+
+        for (var i = 0; i < saysTo.length; i++) {
+            command = saysTo[i].trim();
+
+            if (saysTo.length > (i + 1)) {
+                argument = saysTo[i + 1].trim();
+            }
+            if (command === 'spotify-this-song' || command === 'movie-this') {
+                i++;
+            }
+
+            start(command);
+        }
+    });
+}
+
+function parseCommand() {
 
 }
 
